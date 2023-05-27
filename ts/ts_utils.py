4dd4375bbedaf448cbb60494eecd2fa7551e5512
tmro98 <125816449+tmro98@users.noreply.github.com>
2023-05-27 23:39:38 +0200

    better grid; pcr and plsr

--- ts/ts_utils.py
+++ ts/ts_utils.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def modOOSR2(y_true, y_pred):
-    numerator = ((y_true - y_pred) ** 2).sum(axis=0, dtype=np.float64)
+    numerator = ((y_true - y_pred.squeeze()) ** 2).sum(axis=0, dtype=np.float64)
     denominator = (y_true**2).sum(axis=0, dtype=np.float64)
     return 1 - (numerator/denominator)
 
 def OOSR2(y_true, y_pred, mean_pred):
-    numerator = ((y_true - y_pred) ** 2).sum(axis=0, dtype=np.float64)
+    numerator = ((y_true - y_pred.squeeze()) ** 2).sum(axis=0, dtype=np.float64)
     denominator = ((y_true - mean_pred)**2).sum(axis=0, dtype=np.float64)
     return 1 - (numerator/denominator)
