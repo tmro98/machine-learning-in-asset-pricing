5b0d61048e08921de37db74b418ace1ce5b3100d
tmro98 <125816449+tmro98@users.noreply.github.com>
2023-09-21 02:03:18 +0200

    ts formatting

--- ts_notebooks/ts_utils.py
+++ ts_notebooks/ts_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-# for testing
+
 def OOSR2(y_true, y_pred, mean_pred):
     numerator = ((y_true - y_pred.squeeze()) ** 2).sum(axis=0, dtype=np.float64)
-    denominator = ((y_true - mean_pred)**2).sum(axis=0, dtype=np.float64)
-    return 1 - (numerator/denominator)
+    denominator = ((y_true - mean_pred) ** 2).sum(axis=0, dtype=np.float64)
+    return 1 - (numerator / denominator)
